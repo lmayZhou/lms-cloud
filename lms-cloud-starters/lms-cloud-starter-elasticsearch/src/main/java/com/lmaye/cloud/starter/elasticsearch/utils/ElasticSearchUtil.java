b9c003caf8200d142d7d95a840da77c61eb54b49
lmay <lmay@lmaye.com>
2023-10-26 18:15:35 +0800

    feat: -- V1.2.12.

--- lms-cloud-starters/lms-cloud-starter-elasticsearch/src/main/java/com/lmaye/cloud/starter/elasticsearch/utils/ElasticSearchUtil.java
+++ lms-cloud-starters/lms-cloud-starter-elasticsearch/src/main/java/com/lmaye/cloud/starter/elasticsearch/utils/ElasticSearchUtil.java
@@ -16,7 +16,7 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchScrollRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
